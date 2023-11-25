896964d3b0a67f0ecaee01f3768b6d056a412f78
Johey Hargrett <joheyhargrett@gmail.com>
2023-11-25 13:41:26 -0500

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    '''function print_fibonacci()'''
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib = [0, 1]
+        fib.extend(fib[i - 1] + fib[i - 2] for i in range(2, length))
+        print(fib)
